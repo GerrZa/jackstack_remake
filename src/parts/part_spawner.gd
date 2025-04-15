d0e78f24f04c357d2f89cd34b98bff081787a922
GerrZa <thanapatr.ger@gmail.com>
2025-04-16 00:14:10 +0700

    juiciness

--- src/parts/part_spawner.gd
+++ src/parts/part_spawner.gd
@@ -2,10 +2,12 @@ extends Node
 
 var circle_ex = preload("res://src/parts/circle_explode.tscn")
 var line_ex = preload("res://src/parts/line_explode.tscn")
+var spr_ex = preload("res://src/parts/spr_explode.tscn")
+var text_float = preload("res://src/parts/text_float.tscn")
 
 func spawn(part_name, glob_pos := Vector2.ZERO, part_attribute := {}):
 	var part_ins = get(part_name).instantiate()
-	part_ins.global_position = glob_pos
+	part_ins.global_position = round(glob_pos)
 	
 	get_tree().current_scene.add_child(part_ins)
 	
